2eab72962218466107b3fb4859e7093d0e41d030
Mark <welshm3@email.chop.edu>
2021-08-10 13:36:16 -0400

    Add gzip support to the SV tool

--- reformat_sv_vcf.py
+++ reformat_sv_vcf.py
@@ -1,5 +1,10 @@
-import argparse, json
+import argparse, gzip, json
 import pandas as pd
+
+
+def is_gz_file(filepath):
+    with open(filepath, 'rb') as test_f:
+        return test_f.read(2) == b'\x1f\x8b'
 
 
 def process_sv_vcf(vcf_lines_list):
@@ -32,11 +37,17 @@ parser.add_argument("--archer_list", dest="archer_list", required=True)
 parser.add_argument("--sample_id", dest="sample_id", required=True)
 args = parser.parse_args()
 
+
 with open(args.archer_list, 'r') as archer_in:
     archer_genes = [x.strip() for x in archer_in.readlines()]
 
-with open(args.vcf_file, 'r') as vcf_in:
-    vcf_lines = vcf_in.readlines()
+# test if VCF is gzipped then deal with it accordingly
+if is_gz_file(args.vcf_file):
+	with gzip.open(args.vcf_file,'r') as gzip_vcf_in:
+    	vcf_lines = gzip_vcf_in.readlines()
+else:
+	with open(args.vcf_file, 'r') as vcf_in:
+    	vcf_lines = vcf_in.readlines()
 
 vcf = process_sv_vcf([x.strip().split('\t') for x in vcf_lines])
 with open('{}.sv_reformated.vcf'.format(args.sample_id), 'w') as fout:
